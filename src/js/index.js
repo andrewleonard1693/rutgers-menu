802835ac93b297949cad82652f5bed1ba23b52dd
Andrew Leonard <andrewleonard1693@gmail.com>
2018-10-03 02:23:24 -0400

    added buttons and added transitions

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 console.log('connected')
+
+
+//handle logic for changing active buttons
+$("#stopsButton").click(function(){
+  removeAllActiveStates();
+  $("#stopsButton").addClass("activeButton");
+  console.log('hello')
+})
+$("#routesButton").click(function(){
+  removeAllActiveStates();
+  $("#routesButton").addClass("activeButton");
+})
+$("#allButton").click(function(){
+  removeAllActiveStates();
+  $("#allButton").addClass("activeButton");
+})
+
+function removeAllActiveStates(){
+  $("#stopsButton").removeClass("activeButton")
+  $("#routesButton").removeClass("activeButton")
+  $("#allButton").removeClass("activeButton")
+}
